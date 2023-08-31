1f49733c975161cc0da8a8aaaa6d1f88fcd10f6e
Al8xShu <107429157+Al8xShu@users.noreply.github.com>
2023-08-31 14:29:15 +0300

    Describe of entity of book.

---
+++ domain/AvSBookStore/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AvSBookStore
+{
+    public class Book
+    {
+        public string Title { get; }
+        public int Id { get; }
+
+        public Book(string title, int id)
+        {
+            Title = title;
+            Id = id;
+        }
+    }
+}
