158197caabd876883ab2edfc10376f5c8a58a0ac
Al8xShu <107429157+Al8xShu@users.noreply.github.com>
2023-12-03 16:33:31 +0300

    _10

---
+++ presentation/AvSBookStore.Web.Contractors/IWebContractorService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AvSBookStore.Web.Contractors
+{
+    public interface IWebContractorService
+    {
+        string UniqCode { get; }
+
+        string GetUri { get; }
+    }
+}
